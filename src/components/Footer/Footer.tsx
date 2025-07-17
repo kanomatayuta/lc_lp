ac8ded14eb8c64843437b569cae79187c101bc01
kanomatayuta <skr7110@icloud.com>
2025-07-17 13:35:45 +0900

    feat: Footerコンポーネントを追加し、メタデータを更新。Headerの下にmainタグを追加。

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Footer.module.scss";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.mainContent}>
+          <div className={styles.logoColumn}>
+            <Link href="/" className={styles.logoLink}>
+              {" "}
+              {/* ★ aタグにもクラス名を付与 */}
+              <Image
+                src="/brand_logo/lc_logo.png"
+                alt="LC Leading Communication Logo"
+                width={200}
+                height={60}
+                style={{ objectFit: "contain", width: "100%", height: "auto" }} // ★インラインスタイルで比率を制御
+              />
+            </Link>
+          </div>
+          <div className={styles.infoColumn}>
+            <h3>会社概要</h3>
+            <p>株式会社 Leading Communication</p>
+            <p>〒 150-0043</p>
+            <p>東京都渋谷区道玄坂1-16-10 渋谷DTビル6F</p>
+            <p>
+              TEL: <a href="tel:03-6427-0997">03-6427-0997</a>
+            </p>
+            <p>
+              Mail: <a href="mailto:info@le-commu.co.jp">info@le-commu.co.jp</a>
+            </p>
+          </div>
+        </div>
+        <div className={styles.copyright}>
+          &copy; {new Date().getFullYear()} Leading Communication
+        </div>
+      </div>
+    </footer>
+  );
+}
