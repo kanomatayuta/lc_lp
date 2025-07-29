d5aea7251c3abb1b122cc9098a89216ccd9e6b0a
kanomatayuta <skr7110@icloud.com>
2025-07-29 10:11:08 +0900

    Google Tag Managerをlayout.tsxに追加し、トラッキングの設定を強化。headセクションにスクリプトを挿入し、noscriptタグを用いて非JavaScript環境への対応を実施。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,7 +78,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K5VFNRD6');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K5VFNRD6"
+            height="0" 
+            width="0" 
+            style={{display:'none',visibility:'hidden'}}
+          />
+        </noscript>
         <ModalProvider>
           {" "}
           {/* ★ アプリ全体をProviderで囲む */}
